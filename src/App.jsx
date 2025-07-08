c974966cd6b8b171b0a59d88d1ae02a90a7f8e33
sahilpopli996 <sahilpopli996@gmail.com>
2025-07-08 18:00:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
+import Roadmap from './components/RoadMap';
+import Footer from './components/Footer';
+
+const App=()=> {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div className="pt-[4.75] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benefits/>
+      <Collaboration/>
+      <Services/>
+      <Pricing/>
+      <Roadmap/>
+      <Footer/>
+
+    </div>
+    <ButtonGradient/>
+    </>
+
+  )
+}
+
+export default App;
